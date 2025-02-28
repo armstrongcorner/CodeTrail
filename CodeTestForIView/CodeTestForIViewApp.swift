9461fcb438ec0381ba9a6239149255af3eb9fe37
armstrongcorner <39514373+armstrongcorner@users.noreply.github.com>
2025-03-01 09:30:22 +1300

    Initial Commit

---
+++ CodeTestForIView/CodeTestForIViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeTestForIViewApp.swift
+//  CodeTestForIView
+//
+//  Created by Armstrong Liu on 01/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct CodeTestForIViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
